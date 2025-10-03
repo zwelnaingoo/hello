88c287087b19c4463ffd67a5503fb94dc5b8fe6c
zwelnaingoo <zwenaingoo@diracetechnology.com>
2025-10-03 10:12:25 +0630

    Test

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -17,6 +17,5 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Admin");
-        System.out.println("Admin");
     }
 }
