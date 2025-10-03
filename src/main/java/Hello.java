d3fa89a9c34ffe7093ad4435c2dc75c767eee1f6
Zwe Naing Oo <25-00329@dat.com>
2025-10-03 09:03:05 +0630

    first commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,20 @@
+/***************************************************************
+ * Author       :	 
+ * Created Date :	
+ * Version      : 	
+ * History  :	
+ * *************************************************************/
+
+/**
+ * Hello Class.
+ * <p>
+ * </p>
+ *
+ * @author
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
